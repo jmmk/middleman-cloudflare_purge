3c92cf1ee10f71eda7d674c60bc4d909d58d2382
Michael McLellan <jmikem825@gmail.com>
2014-03-19 07:02:58 -0600

    Add middleman dependency

--- lib/middleman-cloudflare_purge.rb
+++ lib/middleman-cloudflare_purge.rb
@@ -1,9 +1,11 @@
+require 'middleman-core'
+
 class CloudflarePurge < Middleman::Extension
+    option :force, false, 'Force a full purge'
+
     def initialize(app, options_hash={}, &block)
         super
     end
-
-    alias :included :registered
 end
 
 ::Middleman::Extensions.register(:cloudflare_purge, CloudflarePurge)
--- middleman-cloudflare_purge.gemspec
+++ middleman-cloudflare_purge.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name = 'middleman-cloudflare_purge'
-    s.version = '0.0.1'
+    s.version = '0.0.2'
     s.date = '2014-03-19'
     s.description = 'Purge updated files from your Cloudflare cache'
     s.summary = 'Cloudflare cache purging for Middleman'
